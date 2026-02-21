191fe6c4483daf1d9dcfc7e6e9bbbd1a4a738768
asenatribanyu <asenatribanyu@gmail.com>
2026-02-21 17:28:22 +0700

    refactor: change email for superadimn

--- backend/src/database/seeder/seeder.js
+++ backend/src/database/seeder/seeder.js
@@ -1,10 +1,19 @@
 import fs from "fs";
 import logger from "../../log/logger.js";
-import { db, Role, RolePermission, User, Permission } from "../../model/index.js";
+import {
+  db,
+  Role,
+  RolePermission,
+  User,
+  Permission,
+} from "../../model/index.js";
 import bcrypt from "bcrypt";
 
 const roles = fs.readFileSync("src/database/seeder/role.json", "utf-8");
-const permissions = fs.readFileSync("src/database/seeder/permission.json", "utf-8");
+const permissions = fs.readFileSync(
+  "src/database/seeder/permission.json",
+  "utf-8",
+);
 
 const seed = async () => {
   try {
@@ -14,13 +23,18 @@ const seed = async () => {
     const roleAdmin = await Role.findOne({ where: { name: "Super Admin" } });
     const permissionsData = JSON.parse(permissions);
     for (const permission of permissionsData.permissions) {
-      const createdPermission = await Permission.create({ name: permission.name });
-      await RolePermission.create({ roleId: roleAdmin.id, permissionId: createdPermission.id });
+      const createdPermission = await Permission.create({
+        name: permission.name,
+      });
+      await RolePermission.create({
+        roleId: roleAdmin.id,
+        permissionId: createdPermission.id,
+      });
     }
     const password = await bcrypt.hash("admin123", 10);
     const userAdmin = await User.create({
       name: "Super Admin",
-      email: "asenatribanyu@gmail.com",
+      email: "administrator@gmail.com",
       roleId: roleAdmin.id,
       password,
     });
